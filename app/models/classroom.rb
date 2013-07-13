38aa3769b538832bca892e3bb390b233adbc8497
Kyle Crop <kylecrop@utexas.edu>
2013-07-13 17:08:22 -0500

    Scaffolded all 5 pieces and wrote reference relations in their model files.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,12 @@
 class Classroom
+  # Dependencies
   include Mongoid::Document
+
+  # Relations
+  belongs_to :school
+  has_and_belongs_to_many :courses
+
+  # Properties
   field :name, type: String
-end
+
+end # Classroom
